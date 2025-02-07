b79a864e6461376280e04595c9fbb9aeb218537f
Rose Berger <rberger@colostate.edu>
2025-02-07 13:05:30 -0700

    added final varibales

---
+++ ConnectFour.py
@@ -0,0 +1,9 @@
+BOARD_SIZE = 7
+ROW_SIZE = 6
+WINNING_LENGTH = 4
+
+slots = ['1', '2', '3', '4', '5', '6', '7']
+board = [['*' for x in range(BOARD_SIZE)] for y in range(ROW_SIZE)]
+
+
+print (board)
