1f12f8ff889c83000c1e98d29a6385e8dfeb09d1
Rose Berger <rberger@colostate.edu>
2025-02-07 13:08:13 -0700

    added start game

--- ConnectFour.py
+++ ConnectFour.py
@@ -3,7 +3,18 @@ ROW_SIZE = 6
 WINNING_LENGTH = 4
 
 slots = ['1', '2', '3', '4', '5', '6', '7']
-board = [['*' for x in range(BOARD_SIZE)] for y in range(ROW_SIZE)]
+board = [[' ' for x in range(BOARD_SIZE)] for y in range(ROW_SIZE)]
+
+def print_board():
+    print("  ".join(slots))
+    for row in board:
+        print("* ".join(row))
+
+def start_game():
+    print("Welcome to Connect Four!")
+    
+    #print_board()
+    #player_turn()
 
 
-print (board)
+start_game()
